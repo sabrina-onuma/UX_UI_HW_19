edbb08a05a8c4f37f964b7b7cf5f32f27cc947cd
sabrina-onuma <Sabrina.onuma@gmail.com>
2022-08-04 18:57:12 -0500

    trying li

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#navItem").hover(function(){
+    $("li").hover(function(){
       $(this).css("color", "#B15318");
       }, function(){
       $(this).css("color", "#1c1c1c");
