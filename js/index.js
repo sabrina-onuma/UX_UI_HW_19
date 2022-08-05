62dea7871569a154c603159bc492cda0b9b304a5
sabrina-onuma <Sabrina.onuma@gmail.com>
2022-08-04 19:26:56 -0500

    hover syntax

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $("li").hover(function(){
-    $(this).css("color","#B15318"),
-    $(this).css("color","#1c1c1c")
+    $(this).css("color","#B15318");},
+    function(){
+    $(this).css("color","#1c1c1c");
 
 })
